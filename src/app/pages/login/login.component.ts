ba95d11a4953706a2c13a38f5311d47867182131
mallurivikas <vikasmalluri@gmail.com>
2026-06-13 11:31:21 +0530

    Added Debugging Console And Erros Handling

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,81 @@
-import {Component} from "@angular/core";
-import {ReactiveFormsModule, FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from "@angular/forms";
+import { JsonPipe } from "@angular/common";
 
 @Component({
-    selector:"app-login",
-    templateUrl:"./login.component.html",
-    styleUrls:["./login.component.css"],
-    imports:[ReactiveFormsModule],
-    standalone:true
+  selector: "app-login",
+  standalone: true,
+  imports: [ReactiveFormsModule, JsonPipe],
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css"
 })
+export class LoginComponent {
 
-export class LoginComponent{
-    
-    LoginForm: FormGroup;
+  loginForm!: FormGroup;
 
-    constructor(private fb : FormBuilder) {
-        this.LoginForm = this.fb.group({
-            email:["",[Validators.required,Validators.email]],
-            password:["",[Validators.required,Validators.minLength(6)]]
-        });
+  constructor(private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ]
+
+    });
+
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+        this.loginForm.markAllAsTouched();
+        return;
     }
+    console.log(this.loginForm.value);
 
-    onSubmit(){
-        console.log(this.LoginForm.value);
-    }
+  }
+
+  hasError(controlName: string): boolean {
+
+  const control = this.loginForm.get(controlName);
+
+  return !!(control && control.touched && control.invalid);
+
+}
+
+getErrorMessage(controlName: string): string {
+
+  const control = this.loginForm.get(controlName);
+
+  if (!control?.errors)
+    return "";
+
+  if (control.errors['required'])
+    return `${controlName} is required`;
+
+  if (control.errors['email'])
+    return "Please enter a valid email";
+
+  if (control.errors['minlength'])
+    return `Minimum length is ${control.errors['minlength'].requiredLength}`;
+
+  return "Invalid input";
+
+}
 }
